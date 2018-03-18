fa75642bd62297a6848680c4527a181383739d99
auranbuckles <auran59@gmail.com>
2018-03-18 11:54:09 -0700

    relationships between models and attach uuid to client before create

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,2 +1,11 @@
 class Client < ApplicationRecord
+  before_create :attach_uuid
+  belongs_to :device
+
+  def attach_uuid
+    loop do
+      token = SecureRandom.random_number(100000000..999999999)
+      break token unless Client.where(client_id: token).exists?
+    end
+  end
 end
